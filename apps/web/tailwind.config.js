deb1560e5e7cede756a95cdc4eab928326e93d33
Warpy <anthony@lewarpy.fr>
2025-02-12 23:24:58 +0100

    fix(webapp): directly importing plugin

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,5 +1,3 @@
-import { nextui } from "@nextui-org/theme";
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,5 +10,5 @@ module.exports = {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [import("@nextui-org/theme")],
 };
